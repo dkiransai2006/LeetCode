8b073386b95da02ba5445594b646fa9604b57e2d
D Kiran Sai Kumar <dkiransai2006@gmail.com>
2026-09-20 18:31:47 +0530

    Implement kthFactor function to find k-th factor

---
+++ 1492. The kth Factor of n/Solution.c
@@ -0,0 +1,17 @@
+int kthFactor(int n, int k) {
+    int ans = -1;
+    for(int i = 1; i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+            k -= 1;
+            ans = i;
+
+            if(k == 0)
+            {
+                return ans;
+            }
+        }
+    }
+    return -1;
+}
